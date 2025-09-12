0fd078b5fcee28fd4ece6c784a3154bf055c7b71
arribion <arribion@gmail.com>
2025-09-12 20:58:54 +0300

    set main directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,45 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/arribion-link/arribion.kyu.software.engineering",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+		{
+		  label: "Visual Programming",
+		  autogenerate: { directory: "visual-programming" },
+		},
+        // {
+        //   label: "Web Technologies",
+        //   autogenerate: { directory: "web-development" },
+        // },
+        // {
+        //   label: "Database",
+        //   autogenerate: { directory: "database" },
+        // },
+        // {
+        //   label: "Data Structures and Algorithms",
+        //   autogenerate: { directory: "DSA" },
+        // },
+        // {
+        //   label: "Operating System",
+        //   autogenerate: { directory: "operating-system" },
+        // },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/visual-programming/visual-programming.md
@@ -0,0 +1,4 @@
+---
+title: Introduction to Visual Programming
+decription: Learn the fundamentals of VB6 programming and create your first applications
+---
